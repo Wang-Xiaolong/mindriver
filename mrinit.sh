1c44120f770d9b4f8ba205edce06e865936d7569
Paulo Wong <paulo.dx@gmail.com>
2020-08-19 16:58:31 +0800

    mrinit.sh added

---
+++ mrinit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+[[ $_ == $0 ]] && echo "Only run in sourced mode." && exit
+mr_dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
+[ ! -f "$mr_dir/mr.sh" ] && echo "mr.sh not found." && return
+[ -n "$1" ] && mr_file="$1" || mr_file="$mr_dir/log/wxl.mr"
+. "$mr_dir/mr.sh" init -c mr -e mr -t marktree -f "$mr_file"
+alias mrinit=". $MR_SH init"
+alias mrf=". $MR_SH init -f"
+alias mrclean=". $MR_SH clean"
+alias mra="$MR_SH a"
+alias mre="$MR_SH e"
+alias mrm="$MR_SH m"
+alias mrl="$MR_SH l"
+alias mrlv="$MR_SH l -v"
+alias mrls="$MR_SH ls"
+unset mr_dir mr_file
