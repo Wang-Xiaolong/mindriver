a3d5af90054a326f85f20ecc3fe1169e098fa1d2
Paulo Wong <paulo.dx@gmail.com>
2020-09-12 11:38:26 +0800

    mrinit.sh sync

--- mrinit.sh
+++ mrinit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 [[ $_ == $0 ]] && echo "Only run in sourced mode." && exit
-mr_dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
-[ ! -f "$mr_dir/mr.sh" ] && echo "mr.sh not found." && return
-[ -n "$1" ] && mr_id="$1"
-. "$mr_dir/mr.sh" -c mr -i "$mr_id"
-alias mri=". $MR_SH -i"
+dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
+[ ! -f "$dir/mr.sh" ] && echo "mr.sh not found." && unset dir && return
+[ -n "$1" ] && file="$1" || file=0
+. "$dir/mr.sh" -c mr -f "$file"; unset dir file
+alias mrf=". $MR_SH -f"
 alias mrclean=". $MR_SH clean"
 alias mra="$MR_SH a"
 alias mre="$MR_SH e"
@@ -12,4 +12,3 @@ alias mrm="$MR_SH m"
 alias mrl="$MR_SH l"
 alias mrlv="$MR_SH l -v"
 alias mrls="$MR_SH ls"
-unset mr_dir mr_id
