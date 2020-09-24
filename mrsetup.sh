5e8a2357a7f398e8695fd213117f45406c686004
Paulo Wong <paulo.dx@gmail.com>
2020-09-24 08:49:15 +0800

    mrsetup: no longer specify a file by default

--- mrsetup.sh
+++ mrsetup.sh
@@ -2,8 +2,8 @@
 [[ $_ == $0 ]] && echo "Only run in sourced mode." && exit
 dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
 [ ! -f "$dir/mr.sh" ] && echo "mr.sh not found." && unset dir && return
-[ -n "$1" ] && file="$1" || file=0
-. "$dir/mr.sh" -c mr -f "$file" -p; unset dir file
+[ -n "$1" ] && . "$dir/mr.sh" -c mr -f "$1" -p || . "$dir/mr.sh" -c mr -p
+unset dir
 alias mr0=". $MR_SH"
 alias mrf=". $MR_SH -f"
 alias mrclean=". $MR_SH clean"
