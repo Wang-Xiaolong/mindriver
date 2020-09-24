f239ba300b9b445cdf78bf92dc586ffa8ccf8e69
Paulo Wong <paulo.dx@gmail.com>
2020-09-24 08:42:19 +0800

    mrinit: sync recent changes

--- mrinit.sh
+++ mrinit.sh
@@ -3,10 +3,13 @@
 dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
 [ ! -f "$dir/mr.sh" ] && echo "mr.sh not found." && unset dir && return
 [ -n "$1" ] && file="$1" || file=0
-. "$dir/mr.sh" -c mr -f "$file"; unset dir file
+. "$dir/mr.sh" -c mr -f "$file" -p; unset dir file
+alias mr0=". $MR_SH"
 alias mrf=". $MR_SH -f"
 alias mrclean=". $MR_SH clean"
+alias mrinit="$MR_SH init"
 alias mra="$MR_SH a"
+alias mrv="$MR_SH v"
 alias mre="$MR_SH e"
 alias mrm="$MR_SH m"
 alias mrl="$MR_SH l"
