e63cd02aee169b492472cca15af614f8960434d7
Paulo Wong <paulo.dx@gmail.com>
2020-11-01 13:38:44 +0800

    list: small change

--- mr.sh
+++ mr.sh
@@ -720,7 +720,7 @@ mr_list() {
 		-n 'mr_list' -- "$@")
 	[ $? -ne 0 ] && echo "Failed parsing the arguments." && return
 	eval set -- "$PARAMS"; debug "mr_list($@)"
-	local n=false v=false fr='' to='' d="." s="" r='' depth='-maxdepth 1'
+	local n=false v=false fr='' to='' d=. s='' r='' depth='-maxdepth 1'
 	while : ; do
 		case "$1" in
 		-n|--mono) n=true; shift;;
